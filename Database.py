a4b7e7aa8a7efd5ced657b4ecab10c7ba11ccb53
Irgendwer008 <47567520+Irgendwer008@users.noreply.github.com>
2025-10-07 22:50:14 +0200

    correct select statement

--- Database.py
+++ Database.py
@@ -17,19 +17,17 @@ compartements = [(1, "M6 Screws"), (1, "M5 Screws"), (2, "Screwdriver"), (3, "M5
 
 for i in range(compartements.__len__()):
     compartements[i] = (compartements[i][0], compartements[i][1], i * 3, 3)
-    
-print(compartements)
 
 cursor.executemany("insert into shelves (label) values (?)", shelves)
 cursor.executemany("insert into compartements (shelf, cargo, position, length) values (?, ?, ?, ?)", compartements)
 
 #print
 
-for row in cursor.execute("select id, * from shelves").fetchall():
-    print(row[1], ": ", end="")
+for row in cursor.execute("select * from shelves").fetchall():
+    print(f"{row[1]} || ", end="")
     
     for col in cursor.execute("select * from compartements where shelf = ?", (row[0],)).fetchall():
-        print(col[0], ": ", col[2], " | ", end="")
+        print(f"{col[0]}: {col[2]} | ", end="")
     
     print("")
 
