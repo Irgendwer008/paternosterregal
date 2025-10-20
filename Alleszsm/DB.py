3f0ed43e32351d479d08af96899cc9bf7db05c96
Irgendwer008 <47567520+Irgendwer008@users.noreply.github.com>
2025-10-20 23:28:30 +0200

    remove double reset procedure

--- Alleszsm/DB.py
+++ Alleszsm/DB.py
@@ -6,23 +6,7 @@ class DB:
         self.connection.execute("PRAGMA foreign_keys = ON")
         self.cursor = self.connection.cursor()
         
-        self.cursor.execute("DROP TABLE IF EXISTS shelves")
-        self.cursor.execute("DROP TABLE IF EXISTS compartments")
-        
-        self.cursor.execute("create table shelves (id integer primary key, label text)")
-        self.cursor.execute("create table compartments (id integer primary key, shelf integer, cargo text, position integer, length integer, foreign key (shelf) references shelves(id) on delete cascade)")
-
-        shelves = [("A",), ("B",), ("C",)]
-        compartments = [(1, "M6 Screws"), (1, "M5 Screws"), (2, "Screwdriver"), (3, "M5 Nut"), (3, "M6 Nut"), (3, "M7 Nut")]
-
-
-        for i in range(compartments.__len__()):
-            compartments[i] = (compartments[i][0], compartments[i][1], i * 3, 3)
-
-        self.cursor.executemany("insert into shelves (label) values (?)", shelves)
-        self.cursor.executemany("insert into compartments (shelf, cargo, position, length) values (?, ?, ?, ?)", compartments)
-
-        self.connection.commit()
+        self.reset()
     
     def to_string(self):
         string = ""
