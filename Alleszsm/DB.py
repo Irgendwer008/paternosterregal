22a5997f9f1830a65626446d81abef7b807b1eee
Irgendwer008 <47567520+Irgendwer008@users.noreply.github.com>
2025-10-22 23:28:50 +0200

    Automatically populate shelves

--- Alleszsm/DB.py
+++ Alleszsm/DB.py
@@ -1,10 +1,12 @@
 import sqlite3
+from xlsxwriter.utility import xl_col_to_name
 
 class DB:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, number_of_shelves: int = 10):
         self.connection = sqlite3.connect(filename)
         self.connection.execute("PRAGMA foreign_keys = ON")
         self.cursor = self.connection.cursor()
+        self.number_of_shelves = number_of_shelves
         
         self.reset()
     
@@ -29,16 +31,16 @@ class DB:
         self.cursor.execute("DROP TABLE IF EXISTS shelves")
         self.cursor.execute("DROP TABLE IF EXISTS compartments")
 
-        self.cursor.execute("create table shelves (id integer primary key, label text)")
+        self.cursor.execute("create table shelves (id integer primary key, label text, position integer)")
         self.cursor.execute("create table compartments (id integer primary key, shelf integer, cargo text, position integer, length integer, foreign key (shelf) references shelves(id) on delete cascade)")
 
-        shelves = [("A",), ("B",), ("C",)]
+        shelves = [(xl_col_to_name(i), i * 800/3) for i in range(self.number_of_shelves)]
         compartments = [(1, "M6 Screws"), (1, "M5 Screws"), (2, "Screwdriver"), (3, "M5 Nut"), (3, "M6 Nut"), (3, "M7 Nut")]
 
         for i in range(compartments.__len__()):
             compartments[i] = (compartments[i][0], compartments[i][1], i * 6 + 3, 5)
 
-        self.cursor.executemany("insert into shelves (label) values (?)", shelves)
+        self.cursor.executemany("insert into shelves (label, position) values (?, ?)", shelves)
         self.cursor.executemany("insert into compartments (shelf, cargo, position, length) values (?, ?, ?, ?)", compartments)
 
         self.connection.commit()
